6e5079194864abb91ddd18f11a5719f84c296447
Logan <loganxlow@gmail.com>
2015-08-27 17:34:54 -0700

    Added routing for signin

--- client/app.js
+++ client/app.js
@@ -12,6 +12,10 @@ var app = angular.module('main', ['firebase', 'ui.router'])
       url: '/signup',
       templateUrl: './app/signup/signup.html'
     })
+    .state('signin', {
+      url: '/signin',
+      templateUrl: './app/signin/signin.html'
+    })
     .state('userProfile', {
       url: '/userProfile',
       templateUrl: './app/userProfile/userProfile.html'
