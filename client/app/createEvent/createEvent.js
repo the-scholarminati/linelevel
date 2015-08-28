1679c2de76e535be631558b4f46cea3d16945f77
dierat <diedra.rater@gmail.com>
2015-08-27 22:24:42 -0700

    adds basic form with genres menu that allows users to select one or more genres

--- client/app/createEvent/createEvent.js
+++ client/app/createEvent/createEvent.js
@@ -1,7 +1,39 @@
 //attaching controllers to main until we find reason to create specific modules
 
-angular.module('main').controller('createEventController',[
-  function(){
+angular.module('main')
+
+.controller('createEventController', ['$scope', 
+  function($scope){
+
+    // move to a factory/service so it can be used by users when they're creating their profile as well
+    var genres = ['Classical', 'Jazz', 'Pop', 'Rock', 'Blues', 'Folk', 'Country', 'Electronic', 'Experimental'];
+    $scope.genres = genres.reduce(function(array, genre){
+      array.push( {name: genre, selected: false} );
+      return array;
+    }, []);
+
+    $scope.chosenGenres = [];
+    $scope.chooseGenre = function(genre){
+      genre.selected = !genre.selected;
+
+      if (genre.selected){
+        $scope.chosenGenres.push(genre);
+      } else {
+        var indexOfGenre = $scope.chosenGenres.indexOf(genre);
+        $scope.chosenGenres.splice(indexOfGenre, 1);
+      }
+      console.log("$scope.chosenGenres = ", JSON.stringify($scope.chosenGenres));
+    };
+
+    $scope.submitCreateEventForm = function(){
+      // push event data to the database
+
+      // the image url is not required on the form
+      // maybe have a stand-in image that is used for events that don't have this field
+
+      // console log to test the button is functioning
+      console.log("event creation form submitted!");
+    };
 
   }
 ]);
