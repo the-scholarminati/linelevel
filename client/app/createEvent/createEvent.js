e87e0337003c0f490c9015e4dfad6903f1b903a0
dierat <diedra.rater@gmail.com>
2015-08-28 11:18:29 -0700

    moves chooseGenre method to factory

--- client/app/createEvent/createEvent.js
+++ client/app/createEvent/createEvent.js
@@ -5,29 +5,29 @@ angular.module('main')
 .controller('createEventController', ['$scope', 'appFactory', 
   function($scope, appFactory){
 
+    // saves the genre lists and method from the factory so we can access them in the DOM
     $scope.genres = appFactory.genres;
-
-    $scope.chosenGenres = [];
-    $scope.chooseGenre = function(genre){
-      genre.selected = !genre.selected;
-
-      if (genre.selected){
-        $scope.chosenGenres.push(genre);
-      } else {
-        var indexOfGenre = $scope.chosenGenres.indexOf(genre);
-        $scope.chosenGenres.splice(indexOfGenre, 1);
-      }
-      console.log("$scope.chosenGenres = ", JSON.stringify($scope.chosenGenres));
-    };
+    $scope.chosenGenres = appFactory.chosenGenres;
+    $scope.chooseGenre = appFactory.chooseGenre;
 
     $scope.submitCreateEventForm = function(){
-      // push event data to the database
-
+      // saves the data from the form and clears the fields
+      var eventTitle = $scope.eventTitle;
+      var eventDescription = $scope.eventDescription;
       // the image url is not required on the form
-      // maybe have a stand-in image that is used for events that don't have this field
+      // maybe have a default image that is used when image is not provided
+      var eventImage = $scope.eventImage;
+      var eventLabel = $scope.eventLabel;
+
 
       // console log to test the button is functioning
       console.log("event creation form submitted!");
+
+
+      // TODO: push event data to the database
+
+
+      // what happens after an event is successfully created? redirect to the user's profile?
     };
 
   }
--- client/factory.js
+++ client/factory.js
@@ -3,11 +3,27 @@ angular.module('main')
 .factory('appFactory', function(){
   var obj = {};
 
+
   var genres = ['Classical', 'Jazz', 'Pop', 'Rock', 'Blues', 'Folk', 'Country', 'Electronic', 'Experimental'];
   obj.genres = genres.reduce(function(array, genre){
     array.push( {name: genre, selected: false} );
     return array;
   }, []);
+
+  obj.chosenGenres = [];
+
+  obj.chooseGenre = function(genre){
+    genre.selected = !genre.selected;
+
+    if (genre.selected){
+      obj.chosenGenres.push(genre);
+    } else {
+      var indexOfGenre = obj.chosenGenres.indexOf(genre);
+      obj.chosenGenres.splice(indexOfGenre, 1);
+    }
+    console.log("obj.chosenGenres = ", JSON.stringify(obj.chosenGenres));
+  };
+
 
   return obj;
 });
