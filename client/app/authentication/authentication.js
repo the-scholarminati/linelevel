1bfcbcbbffad0355d979179daa442b0c7ae5bfcf
dierat <diedra.rater@gmail.com>
2015-08-28 18:35:53 -0700

    refactors signup/in pages to use forms instead of fieldsets and formgroups and ng-submit instead of ng-click because the methods weren't being called before

--- client/app/authentication/authentication.js
+++ client/app/authentication/authentication.js
@@ -1,21 +1,22 @@
+//attaching controllers to main until we find reason to create specific modules
+
 angular.module('main')
 
-.controller('authController', ['$http','$scope', 
-  function($scope, $http){
-
-    console.log("inside authController");
+.controller('authController', ['$scope', '$http', 'appFactory', 
+  function($scope, $http, appFactory){
 
     $scope.signIn = function(){
       console.log("signIn form submitted!");
 
       // $http.post('/auth/signin',$scope.credentials);
     };
+
     $scope.signUp = function(){
-      console.log("$scope.credentials.firstname = ", $scope.credentials.firstname);
 
       console.log("signUp form submitted!");
 
       //$http.post('/auth/signup',$scope.credentials);
     };
+
   }
 ]);
