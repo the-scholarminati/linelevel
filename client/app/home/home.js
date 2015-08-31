c1b483fdb2262d0f6ce3374b363cf2cadeb3524e
Alex Rodriguez <al3xelias@gmail.com>
2015-08-31 13:19:13 -0400

    Connect firebase events endpoint Fetch last 20 events

--- client/app/home/home.js
+++ client/app/home/home.js
@@ -4,7 +4,18 @@ angular.module('main')
 
 .controller('homeController', ['$scope', 
   function($scope){
+    $scope.events = [];
+    var ref = new Firebase("https://linelevel.firebaseio.com/");
 
+    //reference events endpoint
+    var eventsRef = ref.child("events");
+
+    //fetch last 20 events
+    eventsRef.limitToLast(20).on('child_added', function(snapshot){
+        var data = snapshot.val();
+        $scope.events.push(data);  //store data in events array
+        console.log(data);
+    });
   }
 ]);
 
