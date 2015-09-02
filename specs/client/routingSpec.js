4e63c70e79bcca5813d9fbcf361b4cb96508ee04
dierat <diedra.rater@gmail.com>
2015-09-02 10:15:27 -0700

    fixes signup route and adds about and signin routes

--- specs/client/routingSpec.js
+++ specs/client/routingSpec.js
@@ -21,17 +21,53 @@ describe('routing', function(){
     // });
   });
 
+  describe('about route', function(){
+    beforeEach(inject(function ($state, $rootScope) {
+      state = $state.get('about');
+    }));
+    
+    it('matches the about path', function () {
+      expect(state.url).toEqual('/about');
+    });
+
+    it('renders the about page', function () {
+      expect(state.templateUrl).toEqual('./app/about/about.html');
+    });
+
+    // it('uses the right controller', function () {
+    //   expect(state.controller).toEqual(...);
+    // });
+  });
+
   describe('signup route', function(){
     beforeEach(inject(function ($state, $rootScope) {
       state = $state.get('signup');
     }));
     
-    it('matches the root path', function () {
+    it('matches the signup path', function () {
       expect(state.url).toEqual('/signup');
     });
 
     it('renders the signup page', function () {
-      expect(state.templateUrl).toEqual('./app/signup/signup.html');
+      expect(state.templateUrl).toEqual('./app/authentication/signup.html');
+    });
+
+    // it('uses the right controller', function () {
+    //   expect(state.controller).toEqual(...);
+    // });
+  });
+
+  describe('signin route', function(){
+    beforeEach(inject(function ($state, $rootScope) {
+      state = $state.get('signin');
+    }));
+    
+    it('matches the signin path', function () {
+      expect(state.url).toEqual('/signin');
+    });
+
+    it('renders the signin page', function () {
+      expect(state.templateUrl).toEqual('./app/authentication/signin.html');
     });
 
     // it('uses the right controller', function () {
@@ -44,7 +80,7 @@ describe('routing', function(){
       state = $state.get('userProfile');
     }));
     
-    it('matches the root path', function () {
+    it('matches the userProfile path', function () {
       expect(state.url).toEqual('/userProfile/:userId');
     });
 
@@ -62,7 +98,7 @@ describe('routing', function(){
       state = $state.get('createEvent');
     }));
     
-    it('matches the root path', function () {
+    it('matches the createEvent path', function () {
       expect(state.url).toEqual('/createEvent');
     });
 
@@ -80,7 +116,7 @@ describe('routing', function(){
       state = $state.get('event');
     }));
     
-    it('matches the root path', function () {
+    it('matches the event path', function () {
       expect(state.url).toEqual('/event/:eventId');
     });
 
