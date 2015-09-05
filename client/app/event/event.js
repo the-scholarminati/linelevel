0c0151f732ebe20682351d1f6ec0a49d55201549
Logan <loganxlow@gmail.com>
2015-09-05 15:20:15 -0700

    Restored lost commits

--- client/app/event/event.js
+++ client/app/event/event.js
@@ -7,8 +7,11 @@ angular.module('main').controller('eventController',['$scope','$http', 'appFacto
     // console.log("Loading event page...");
     $scope.chatVisible = true;
     $scope.event = {};
+    $scope.chat = $('#chatmessages');
     $scope.event.messages = [];
-    var chatEl = document.getElementById('chatMessages');
+    $scope.chat.scrollTop = $scope.chat.scrollHeight;
+    $scope.isSameUser = false;
+
     // window.console.log('eventId', $scope.eventId);
 
     //instantiate firbase ref with url
@@ -16,8 +19,8 @@ angular.module('main').controller('eventController',['$scope','$http', 'appFacto
     var initialized2 = false;
     var ref = appFactory.firebase;
     var userData = '';
-
     var chatRef = '';
+    var chatEl = $('#chatmessages');
 
     // initialize controller
     var init = function(){
@@ -26,34 +29,37 @@ angular.module('main').controller('eventController',['$scope','$http', 'appFacto
         ref.off();
 
         ref.child("events").child($scope.eventId)
-          .on("value",function(info){
-            window.console.log('checking event info ', info.val());
+          .on("value",function(info){ 
             var eventData = info.val();
-            appFactory.update($scope,function(scope){
-              scope.event.host = eventData.host;
-              scope.event.name = eventData.title;
-            });
+            console.log(eventData);
+            $scope.event.host = eventData.host;
+            $scope.event.name = eventData.title;
+            $scope.event.videoId = eventData.videoId;
+            $scope.isSameUser = appFactory.user === $scope.event.host ? true : false;
+          console.log(appFactory.user +  $scope.event.host + $scope.isSameUser);
         });
       }// end of if
     };
+
 
     var init2 = function(){
       if(!initialized2){
         initialized2 = !initialized2;
 
-        var userAuth = ref.getAuth();
-        if(userAuth){
-          window.console.log('userAuth is ', userAuth);
-          ref.child("users").child(userAuth.uid).on("value",function(user){
-            userData = user.val();
-          });
-        }
+        // var userAuth = ref.getAuth();
+        // ref.child("users").child(userAuth.uid).on("value",function(user){
+        //   userData = user.val();
+        // });
 
         chatRef = ref.child("chats").child($scope.eventId);
         chatRef.on('child_added', function(message){
-          appFactory.update($scope,function(){
+          if(!$scope.$$phase){
+            $scope.$apply(function(){
+              $scope.event.messages.push(message.val());
+            });
+          } else {
             $scope.event.messages.push(message.val());
-          });
+          }
         });
       }
     };
@@ -80,15 +86,76 @@ angular.module('main').controller('eventController',['$scope','$http', 'appFacto
       },30);
     });
 
+     $scope.startStream = function(){
+      var peer = new Peer({key: '66p1hdx8j2lnmi',
+                          debug: 3,
+                          config: {'iceServers': [
+                          {url: 'stun:stun.l.google.com:19302'},
+                          {url: 'stun:stun1.l.google.com:19302'}
+                          ]}
+                        });
+
+      peer.on('open', function(){
+        ref.child("events").child($scope.eventId).update({'videoId' : peer.id});
+        console.log(peer.id);
+
+      });
+
+      peer.on( 'connection', function(conn) {
+        conn.on( 'open', function() {
+          var call = peer.call(conn.peer, window.localStream);
+        });
+      });
+      
+      navigator.getUserMedia = ( navigator.getUserMedia ||
+                              navigator.webkitGetUserMedia  ||
+                              navigator.mozGetUserMedia ||
+                              navigator.msGetUserMedia );
+
+      // get audio/video
+      navigator.getUserMedia({audio:true, video: true}, function (stream) {
+          //display video
+          var video = document.getElementById("myVideo");
+          console.log(URL.createObjectURL(stream));
+          video.src = URL.createObjectURL(stream);
+          window.localStream = stream;
+        },
+        function (error) { console.log(error); }
+      );
+
+    };
+
+    $scope.loadStream = function(){
+      var peer = new Peer({key: '66p1hdx8j2lnmi'});
+      console.log('video id ' + $scope.event.videoId);
+
+      var conn = peer.connect('r12905iwdjbp4x6r');
+
+      peer.on('call', function (incomingCall) {
+        incomingCall.answer(null);
+        incomingCall.on('stream', function(stream){
+          var video = document.getElementById("theirVideo");
+          console.log('create object url' + URL.createObjectURL(stream));
+          video.src = URL.createObjectURL(stream);
+        });
+      });
+    };
+    $scope.$watch('$scope.event.videoId', function loadVideo(a,b){
+      if($scope.isSameUser !== true){
+        $scope.loadStream();
+      }
+    });
+
+
     $scope.toggleChat = function(){
       // console.log($scope.chatVisible);
       $scope.chatVisible = !$scope.chatVisible;
     };
 
   //  ------------------------YOUTUBE STUFF ----------------------
-    $scope.startStream = function(){
-      appFactory.startStream($scope.accessToken);
-    };
+    // $scope.startStream = function(){
+    //   appFactory.startStream($scope.accessToken);
+    // };
 
     $scope.auth = function(){
       var config = {
