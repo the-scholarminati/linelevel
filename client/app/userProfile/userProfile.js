c422f6d1fcecf32a628a75723134d09f8a439292
Alex Rodriguez <al3xelias@gmail.com>
2015-09-07 17:02:25 -0400

    Change firebase event handler to fetch data by user ID -Not complete, call back not populating data array in time for display on html page

--- client/app/userProfile/userProfile.js
+++ client/app/userProfile/userProfile.js
@@ -6,14 +6,23 @@ angular.module('main')
   function($scope, appFactory){
     if(appFactory.auth() === true){
       $scope.userData = [];
+
       var ref = appFactory.firebase;
-      var userRef = ref.child('users').child(ref.getAuth().uid);
-      console.log(ref.getAuth().uid);
+
+      var userRef = ref.child('usernames').child($scope.userId);
 
       userRef.once('value', function(snap) {
-        console.log('I fetched a user!', snap.val());
-        $scope.userData.push(snap.val());
+        console.log('I fetched a user!', snap.val().uid);
+
+        var userProfile = ref.child('users').child(snap.val().uid);
+    
+        userProfile.once('value', function(snap){
+          console.log(snap.val().username);
+          $scope.userData.push(snap.val());
+          console.log($scope.userData);
+        });
       });
+
 
     }
 
