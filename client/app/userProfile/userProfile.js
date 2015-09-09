4cfbda641e01f3a883970297500dc015a61e05b8
Alex Rodriguez <al3xelias@gmail.com>
2015-09-09 19:24:31 -0400

    Retrieve events using eventIds stored in currentEvents field for user in firebase

--- client/app/userProfile/userProfile.js
+++ client/app/userProfile/userProfile.js
@@ -7,21 +7,35 @@ angular.module('main')
 
     if(appFactory.auth() === true){
       $scope.uData = {};
+      $scope.uData.events = [];
+      $scope.uData.eventIds = [];
 
       var ref = appFactory.firebase;
 
-      var userRef = ref.child('usernames').child($scope.userName);
+      var userRef = ref.child('usernames').child($scope.userName); //$scope.userName
       userRef.on('value', function(snap) {
         console.log('I fetched a user!', snap.val().uid);
 
         var userProfile = ref.child('users').child(snap.val().uid);
         userProfile.on('value', function(snap){
-          $scope.uData.username = snap.val().username;
-          $scope.uData.firstname = snap.val().firstname;
-          $scope.uData.lastname = snap.val().lastname;
-          $scope.uData.email = snap.val().email;
-          $scope.uData.genres = snap.val().chosenGenres;
-          $scope.uData.myEvents = snap.val().currentEvents;
+          appFactory.update($scope, function(scope){
+            scope.uData.username = snap.val().username;
+            scope.uData.firstname = snap.val().firstname;
+            scope.uData.lastname = snap.val().lastname;
+            scope.uData.email = snap.val().email;
+            scope.uData.genres = snap.val().chosenGenres;
+            scope.uData.eventIds = snap.val().currentEvents;
+
+            var eventIds = scope.uData.eventIds;
+            console.log(scope.uData);
+            angular.forEach(eventIds, function(eventId){
+              ref.child('events').child(eventId).on('value', function(snap){
+                scope.uData.events = snap.val();
+                console.log(scope.uData.events);
+                
+              });
+            });
+          });
         });
       });
     }
