6b7a0bf4222d1013291e3a4568e550d2b8b3f703
Alex Rodriguez <al3xelias@gmail.com>
2015-09-09 19:23:02 -0400

    SaveDate method modified to push data for easier retrieval

--- client/app/home/home.js
+++ client/app/home/home.js
@@ -33,13 +33,9 @@ angular.module('main')
     });
 
     $scope.saveDate = function(eventKey){
-      var list = [];
-      var userRef = ref.child('users').child('currentEvents');
-      userRef.on('value', function(snap){
-        list = snap.val();
-      });
-      list.push(eventKey);
-      userRef.set(list);
+      $scope.list = [];
+      userRef = ref.child('users').child(ref.getAuth().uid);
+      userRef.child('currentEvents').push(eventKey);
     };
 
 
