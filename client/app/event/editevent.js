3f706348af90e5f34b5283ada72afe978a5443a5
Charles Crame <ccrame@gmail.com>
2015-09-14 00:15:26 -0700

    editEvent page: fixed genres bug and fixed indentation

--- client/app/event/editevent.js
+++ client/app/event/editevent.js
@@ -6,7 +6,7 @@ angular.module('main').controller('editEventController',['$scope','appFactory', 
   var user = ref.getAuth();
 
   // this is the list of the user's chosen genres
-  var genres = appFactory.genres;
+  $scope.genres = appFactory.genres;
   $scope.chosenGenres = appFactory.chosenGenres;
   $scope.chooseGenre = appFactory.chooseGenre;
 
@@ -17,79 +17,74 @@ angular.module('main').controller('editEventController',['$scope','appFactory', 
 
     // sets the date to today and time to 7pm with no seconds
   appFactory.resetDate();
-  
+
+  window.test = function(){
+    console.log('chosen ', $scope.chosenGenres);
+    console.log('appFactory ', appFactory.chosenGenres);
+    console.log('genres ', $scope.genres);
+  };
  
 
   var init = function(){
       // load event data
-      ref.child("events").child($scope.eventId)
-        .on("value",function(info){
+    ref.child("events").child($scope.eventId)
+      .on("value",function(info){
 
-          var eventData = info.val();
-          appFactory.update($scope,function(scope){
-            $scope.eventTitle = eventData.title;
-            $scope.eventDescription = eventData.description;
-            $scope.eventImage = eventData.image;
-            $scope.eventLabel = eventData.label;
-            $scope.genre = eventData.genre;
-            $scope.private = eventData.private;
-            $scope.followersOnly = eventData.followersOnly;
-          });
-
-          if($scope.genre){
-            $scope.genre.forEach(function(genre){
-              $scope.chosenGenres.push(genre);
-              genres.forEach(function(genres){
-                if(genres.name === genre){
-                  genres.selected = true;
-                }
-              });
-            });
-          }
-
-          $scope.genres = genres;
-          $scope.date.eventDate = new Date(eventData.date);
-          // console.log($scope.genres);
-          // console.log($scope.genre);
+        var eventData = info.val();
+        appFactory.update($scope,function(scope){
+          $scope.eventTitle = eventData.title;
+          $scope.eventDescription = eventData.description;
+          $scope.eventImage = eventData.image === './assets/albumcover.png' ? '' : eventData.image;
+          $scope.eventLabel = eventData.label;
+          appFactory.chosenGenres = eventData.genre || [];
+          $scope.private = eventData.private;
+          $scope.followersOnly = eventData.followersOnly;
         });
-        
-    };
 
-    $scope.saveChanges = function(){
-      console.log('save changes invoked');
-      $scope.eventTitle =this.eventTitle;
-      var eventTitle = this.eventTitle;
-      var eventDescription = this.eventDescription;
-
-      // the image url is not required on the form
-      // maybe have a default image that is used when image is not provided
-      var eventImage = this.eventImage || './assets/albumcover.png';
-      var eventLabel = this.eventLabel || '';
-      var eventDate = $scope.date.eventDate.getTime();
-      console.log(eventTitle);
-      console.log(eventDescription);
-      var chosenGenres = this.chosenGenres;
-      var privateEvent = this.private;
-      var followersOnly = this.followersOnly;
-
-      console.log('chosen genres' + chosenGenres);
-
-
-      ref.child("events").child($scope.eventId).update({
-            'title': eventTitle,
-            'description': eventDescription,
-            'image': eventImage,
-            'label': eventLabel,
-            'date': eventDate,
-            'genre': chosenGenres,
-            'private': privateEvent,
-            'followersOnly': followersOnly
+        $scope.date.eventDate = new Date(eventData.date);
+        // console.log($scope.genres);
+        // console.log($scope.genre);
+        appFactory.update($scope,function(scope){
+          $scope.genres.forEach(function(genre){
+            appFactory.chosenGenres.forEach(function(item){
+              genre.name === item ? genre.selected = true : 0;
+            });
           });
+        });
+    });
+  };
 
+  $scope.saveChanges = function(){
+    console.log('save changes invoked');
+    $scope.eventTitle =this.eventTitle;
+    var eventTitle = this.eventTitle;
+    var eventDescription = this.eventDescription;
 
-     window.location.href="#/event/" + $scope.eventId;
+    // the image url is not required on the form
+    // maybe have a default image that is used when image is not provided
+    var eventImage = this.eventImage || './assets/albumcover.png';
+    var eventLabel = this.eventLabel || '';
+    var eventDate = $scope.date.eventDate.getTime();
+    console.log(eventTitle);
+    console.log(eventDescription);
+    var privateEvent = this.private;
+    var followersOnly = this.followersOnly;
 
-    };
+    var eventRef = ref.child("events").child($scope.eventId);
+    eventRef.update({
+      'title': eventTitle,
+      'description': eventDescription,
+      'image': eventImage,
+      'label': eventLabel,
+      'date': eventDate,
+      'private': privateEvent,
+      'followersOnly': followersOnly
+    });
+    eventRef.child("genre").set(appFactory.chosenGenres);
+
+    appFactory.resetGenres();
+    window.location.href="#/event/" + $scope.eventId;
+  };
 
 
   $scope.isAuth = function(){
